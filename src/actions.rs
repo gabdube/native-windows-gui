e9a0deaab9054ae51be888df4c6e2e171ac1eeb9
gabdube <gdube.475@gmail.com>
2016-10-15 12:58:31 -0400

    license & version bump

--- src/actions.rs
+++ src/actions.rs
@@ -1,9 +1,25 @@
 /*!
     Actions is an enumeration of any actions that is possible to do 
     on the controls. 
-
-    No controls implement all Actions.
 */
+/*
+    Copyright (C) 2016  Gabriel Dubé
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 use std::hash::Hash;
 use constants::{CheckState, WindowDisplay, MessageButtons, MessageIcons, ControlType, MessageChoice};
 
