7ea0ac7eb557b2153894ca5d54e7735acd6c92ec
gdube <gdube.475@gmail.com>
2019-11-25 21:30:41 -0500

    calculator buttons

--- native-windows-gui/examples/calculator.rs
+++ native-windows-gui/examples/calculator.rs
@@ -1,6 +1,5 @@
 /*!
     A calculator that use the grid layout of NWG.
-    It does not actually do anything though...
 */
 
 extern crate native_windows_gui as nwg;
@@ -10,6 +9,28 @@ use nwg::NativeUi;
 #[derive(Default)]
 pub struct Calculator {
     window: nwg::Window,
+
+    //layout: GridLayout,
+
+    input: nwg::TextInput,
+
+    btn0: nwg::Button,
+    btn1: nwg::Button,
+    btn2: nwg::Button,
+    btn3: nwg::Button,
+    btn4: nwg::Button,
+    btn5: nwg::Button,
+    btn6: nwg::Button,
+    btn7: nwg::Button,
+    btn8: nwg::Button,
+    btn9: nwg::Button,
+
+    btn_plus: nwg::Button,
+    btn_minus: nwg::Button,
+    btn_mult: nwg::Button,
+    btn_divide: nwg::Button,
+    btn_process: nwg::Button,
+    btn_clear: nwg::Button,
 }
 
 impl Calculator {
@@ -39,10 +60,35 @@ mod calculator_ui {
             
             // Controls
             nwg::Window::builder()
-                .size((300, 315))
+                .size((300, 150))
                 .position((300, 300))
-                .title("The calculator")
+                .title("Calculator")
                 .build(&mut data.window)?;
+
+            nwg::TextInput::builder()
+                .text("")
+                .readonly(true)
+                .parent(&data.window)
+                .build(&mut data.input)?;
+
+            nwg::Button::builder().text("0").parent(&data.window).build(&mut data.btn0)?;
+            nwg::Button::builder().text("1").parent(&data.window).build(&mut data.btn1)?;
+            nwg::Button::builder().text("2").parent(&data.window).build(&mut data.btn2)?;
+            nwg::Button::builder().text("3").parent(&data.window).build(&mut data.btn3)?;
+            nwg::Button::builder().text("4").parent(&data.window).build(&mut data.btn4)?;
+            nwg::Button::builder().text("5").parent(&data.window).build(&mut data.btn5)?;
+            nwg::Button::builder().text("6").parent(&data.window).build(&mut data.btn6)?;
+            nwg::Button::builder().text("7").parent(&data.window).build(&mut data.btn7)?;
+            nwg::Button::builder().text("8").parent(&data.window).build(&mut data.btn8)?;
+            nwg::Button::builder().text("9").parent(&data.window).build(&mut data.btn9)?;
+
+            nwg::Button::builder().text("+").parent(&data.window).build(&mut data.btn_plus)?;
+            nwg::Button::builder().text("-").parent(&data.window).build(&mut data.btn_minus)?;
+            nwg::Button::builder().text("*").parent(&data.window).build(&mut data.btn_mult)?;
+            nwg::Button::builder().text("/").parent(&data.window).build(&mut data.btn_divide)?;
+            nwg::Button::builder().text("Done").parent(&data.window).build(&mut data.btn_process)?;
+            nwg::Button::builder().text("Clear").parent(&data.window).build(&mut data.btn_clear)?;
+            
 
             // Wrap-up
             let ui = Rc::new(CalculatorUi { inner: data });
@@ -64,6 +110,30 @@ mod calculator_ui {
 
                 nwg::bind_event_handler(handle, handle_events);
             }
+
+            // Layouts
+            nwg::GridLayout::builder()
+                .parent(&ui.window)
+                .spacing(2)
+                .min_size([150, 140])
+                .child_item(nwg::GridLayoutItem::new(&ui.input, 0, 0, 5, 1))
+                .child(0, 1, &ui.btn1)
+                .child(1, 1, &ui.btn2)
+                .child(2, 1, &ui.btn3)
+                .child(0, 2, &ui.btn4)
+                .child(1, 2, &ui.btn5)
+                .child(2, 2, &ui.btn6)
+                .child(0, 3, &ui.btn7)
+                .child(1, 3, &ui.btn8)
+                .child(2, 3, &ui.btn9)
+                .child(3, 1, &ui.btn_plus)
+                .child(4, 1, &ui.btn_minus)
+                .child(3, 2, &ui.btn_mult)
+                .child(4, 2, &ui.btn_divide)
+                .child_item(nwg::GridLayoutItem::new(&ui.btn_clear, 3, 3, 2, 1))
+                .child_item(nwg::GridLayoutItem::new(&ui.btn_process, 3, 4, 2, 1))
+                .child_item(nwg::GridLayoutItem::new(&ui.btn0, 0, 4, 3, 1))
+                .build();
             
             return Ok(ui);
         }
