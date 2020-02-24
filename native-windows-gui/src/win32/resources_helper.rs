1ac5c012f2c259007ee6b78589b25767f3c64997
Gabriel D <gdube.475@gmail.com>
2020-02-24 09:36:43 -0500

    move bitmap from memory

--- native-windows-gui/src/win32/resources_helper.rs
+++ native-windows-gui/src/win32/resources_helper.rs
@@ -152,6 +152,66 @@ pub unsafe fn build_oem_image(
 }
 
 
+/** 
+    Create a bitmap from memory.
+    The memory must contain the whole file (including the bitmap header).
+*/
+pub unsafe fn bitmap_from_memory(source: &[u8]) -> Result<HANDLE, NwgError> {
+    use winapi::um::wingdi::{CreateCompatibleBitmap, CreateCompatibleDC, SetDIBits, BITMAPFILEHEADER, BITMAPINFO, BITMAPINFOHEADER, DIB_RGB_COLORS, BI_RGB, RGBQUAD};
+    use winapi::shared::{ntdef::LONG, minwindef::DWORD};
+    use winapi::um::winuser::{GetDC, ReleaseDC};
+    use winapi::ctypes::c_void;
+
+    // Check the header size requirement
+    let fheader_size = mem::size_of::<BITMAPFILEHEADER>();
+    let iheader_size = mem::size_of::<BITMAPINFOHEADER>();
+    let header_size = fheader_size + iheader_size;
+    if source.len() < header_size {
+        let msg = format!("Invalid source. The source size ({} bytes) is smaller than the required headers size ({} bytes).", source.len(), header_size);
+        return Err(NwgError::ResourceCreationError(msg));
+    }
+
+    // Read the bitmap file header
+    let src: *const u8 = source.as_ptr();
+    let fheader_ptr = src as *const BITMAPFILEHEADER;
+    let fheader: BITMAPFILEHEADER = ptr::read( fheader_ptr );
+
+    // Read the bitmap info header
+    let iheader_ptr = src.offset(fheader_size as isize) as *const BITMAPINFOHEADER;
+    let iheader: BITMAPINFOHEADER = ptr::read( iheader_ptr );
+
+    let (w, h) = (iheader.biWidth, iheader.biHeight);
+
+    let screen_dc = GetDC(ptr::null_mut());
+    let hdc = CreateCompatibleDC(screen_dc);
+    let bitmap = CreateCompatibleBitmap(screen_dc, w, h);
+    ReleaseDC(ptr::null_mut(), screen_dc);
+
+    let header = BITMAPINFOHEADER {
+        biSize: mem::size_of::<BITMAPINFOHEADER>() as DWORD,
+        biWidth: w as LONG, biHeight: h as LONG, 
+        biPlanes: 1, biBitCount: 24, biCompression: BI_RGB,
+        biSizeImage: (w * h * 3) as u32,
+        biXPelsPerMeter: 0, biYPelsPerMeter: 0,
+        biClrUsed: 0, biClrImportant: 0
+    };
+
+    let quad = RGBQUAD { rgbBlue: 0, rgbGreen: 0, rgbRed: 0, rgbReserved: 0 };
+    let info = BITMAPINFO {
+        bmiHeader: header,
+        bmiColors: [quad],
+    };
+
+    let data_ptr = source.as_ptr().offset(fheader.bfOffBits as isize) as *const c_void;
+    if 0 == SetDIBits(hdc, bitmap, 0, h as u32, data_ptr, &info, DIB_RGB_COLORS) {
+        let msg = "SetDIBits failed.".to_string();
+        return Err(NwgError::ResourceCreationError(msg));
+    }
+
+    return Ok(bitmap as HANDLE);
+}
+
+
 pub unsafe fn make_bitmap_transparent(handle: HANDLE, size: (i32, i32), key: [u8; 3]) -> Result<HANDLE, NwgError> {
     use winapi::um::wingdi::{RGB, TransparentBlt, CreateCompatibleDC, CreateCompatibleBitmap, DeleteDC, DeleteObject, SelectObject};
     use winapi::shared::windef::{HGDIOBJ};
