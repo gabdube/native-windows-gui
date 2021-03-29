291e3ade24928765d05475ac691a5ddc7b0fd8e7
Gabriel D <gdube.475@gmail.com>
2021-03-28 23:20:37 -0400

    dialog example cleanup

--- native-windows-gui/examples/dialog_multithreading_d.rs
+++ native-windows-gui/examples/dialog_multithreading_d.rs
@@ -15,23 +15,39 @@ use std::{thread, cell::RefCell};
 
 /// The dialog UI
 #[derive(Default, NwgUi)]
-pub struct ThreadingDialog {
-    data: RefCell<String>,
+pub struct YesNoDialog {
+    data: RefCell<Option<String>>,
 
     #[nwg_control(size: (300, 115), position: (650, 300), title: "A dialog", flags: "WINDOW|VISIBLE")]
-    #[nwg_events( OnWindowClose: [ThreadingDialog::close] )]
+    #[nwg_events( OnWindowClose: [YesNoDialog::close] )]
     window: nwg::Window,
 
     #[nwg_control(text: "YES", position: (10, 10), size: (130, 95))]
-    #[nwg_events( OnButtonClick: [ThreadingDialog::choose(SELF, CTRL)] )]
+    #[nwg_events( OnButtonClick: [YesNoDialog::choose(SELF, CTRL)] )]
     choice_yes: nwg::Button,
 
     #[nwg_control(text: "NO", position: (160, 10), size: (130, 95), focus: true)]
-    #[nwg_events( OnButtonClick: [ThreadingDialog::choose(SELF, CTRL)] )]
+    #[nwg_events( OnButtonClick: [YesNoDialog::choose(SELF, CTRL)] )]
     choice_no: nwg::Button,
 }
 
-impl ThreadingDialog {
+impl YesNoDialog {
+
+    /// Create the dialog UI on a new thread. The dialog result will be returned by the thread handle.
+    /// To alert the main GUI that the dialog completed, this function takes a notice sender object.
+    fn popup(sender: nwg::NoticeSender) -> thread::JoinHandle<String> {
+        thread::spawn(move || {
+            // Create the UI just like in the main function
+            let app = YesNoDialog::build_ui(Default::default()).expect("Failed to build UI");
+            nwg::dispatch_thread_events();
+            
+            // Notice the main thread that the dialog completed
+            sender.notice();
+
+            // Return the dialog data
+            app.data.take().unwrap_or("Cancelled!".to_owned())
+        })
+    }
 
     fn close(&self) {
         nwg::stop_thread_dispatch();
@@ -40,9 +56,9 @@ impl ThreadingDialog {
     fn choose(&self, btn: &nwg::Button) {
         let mut data = self.data.borrow_mut();
         if btn == &self.choice_no {
-            *data = "No!".to_string();
+            *data = Some("No!".to_string());
         } else if btn == &self.choice_yes {
-            *data = "Yes!".to_string();
+            *data = Some("Yes!".to_string());
         }
 
         self.window.close();
@@ -79,31 +95,20 @@ impl ThreadingApp {
     }
 
     fn open_dialog(&self) {
-        let mut data = self.dialog_data.borrow_mut();
-        if data.is_some() {
-            nwg::error_message("Error", "The dialog is already running!");
-            return;
-        }
+        // Disable the button to stop the user from spawning multiple dialogs
+        self.button.set_enabled(false);
 
-        let notice = self.dialog_notice.sender();
-
-        *data = Some(thread::spawn(move || {
-            let app = ThreadingDialog::build_ui(Default::default()).expect("Failed to build UI");
-            nwg::dispatch_thread_events();
-            
-            notice.notice();
-            { 
-                let data = app.data.borrow();
-                data.clone()
-            }
-        }))
+        *self.dialog_data.borrow_mut() = Some(YesNoDialog::popup(self.dialog_notice.sender()));
     }
 
     fn read_dialog_output(&self) {
-        let mut data = self.dialog_data.borrow_mut();
-        match data.take() {
+        self.button.set_enabled(true);
+
+        let data = self.dialog_data.borrow_mut().take();
+        match data {
             Some(handle) => {
-                self.name_edit.set_text(&handle.join().unwrap());
+                let dialog_result = handle.join().unwrap();
+                self.name_edit.set_text(&dialog_result);
                 self.button.set_focus();
             },
             None => {}
