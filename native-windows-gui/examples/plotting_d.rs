69f822f85fb08cfa501dbebbb0ac0bca2887c569
Gabriel D <gdube.475@gmail.com>
2021-04-08 21:37:51 -0400

    more plotting

--- native-windows-gui/examples/plotting_d.rs
+++ native-windows-gui/examples/plotting_d.rs
@@ -9,8 +9,8 @@ extern crate native_windows_derive as nwd;
 use nwd::NwgUi;
 use nwg::NativeUi;
 use nwg::stretch::{style::{*, Dimension::*}, geometry::*};
-use std::time::Duration;
-use plotters::prelude::*;
+use std::{cell::RefCell, time::{Duration, Instant}};
+use plotters::{prelude::*};
 
 
 const EXAMPLES: &[&str] = &[
@@ -19,24 +19,41 @@ const EXAMPLES: &[&str] = &[
     "Mandelbrot",
     "Multiple plot",
     "Animated",
+    "Interactive",
+    "Koch Snowflake",
+    "Bitmap",
 ];
+
+#[derive(Default)]
+pub struct PlottingData {
+    animation_start: Option<Instant>
+}
 
 #[derive(Default, NwgUi)]
 pub struct PlottingExample {
+    plotting_data: RefCell<PlottingData>,
+
     #[nwg_control(size: (900, 600), position: (300, 300), title: "Plotting")]
-    #[nwg_events( OnInit: [PlottingExample::draw_graph], OnWindowClose: [nwg::stop_thread_dispatch()] )]
+    #[nwg_events( 
+        OnInit: [PlottingExample::draw_graph],
+        OnWindowClose: [nwg::stop_thread_dispatch()],
+        OnResize: [PlottingExample::draw_graph],
+    )]
     window: nwg::Window,
 
-    #[nwg_layout(parent: window, min_size: [500, 200])]
+    #[nwg_layout(parent: window, min_size: [400, 250])]
     layout: nwg::GridLayout,
 
     #[nwg_control(parent: window)]
-    #[nwg_events( OnResize: [PlottingExample::draw_graph] )]
+    #[nwg_events( 
+        OnMouseMove: [PlottingExample::update_interactive],
+    )]
     #[nwg_layout_item(layout: layout, col: 0, row: 0, col_span: 3)]
     graph: nwg::Plotters,
 
-    #[nwg_control(parent: window, interval: Duration::from_millis(1000/60))]
-    timer: nwg::AnimationTimer,
+    #[nwg_control(parent: window, interval: Duration::from_millis(1000/30))]
+    #[nwg_events( OnTimerTick: [PlottingExample::draw_graph] )]
+    animation_timer: nwg::AnimationTimer,
 
     #[nwg_control(parent: window)]
     #[nwg_layout_item(layout: layout, col: 0, col: 3)]
@@ -55,7 +72,7 @@ pub struct PlottingExample {
     label1: nwg::Label,
 
     #[nwg_control(parent: options_frame, selected_index: Some(0), collection: EXAMPLES.to_vec())]
-    #[nwg_events(OnListBoxSelect: [PlottingExample::draw_graph])]
+    #[nwg_events(OnListBoxSelect: [PlottingExample::check_animate, PlottingExample::draw_graph])]
     #[nwg_layout_item(layout: options_layout, size: Size { width: Auto, height: Points(200.0) })]
     example_list: nwg::ListBox<&'static str>,
 }
@@ -67,12 +84,14 @@ impl PlottingExample {
 
         let mut chart = ChartBuilder::on(&root)
             .caption("y=x^2", ("sans-serif", 50).into_font())
-            .margin(5)
+            .margin(15)
             .x_label_area_size(30)
             .y_label_area_size(30)
             .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32)?;
 
-        chart.configure_mesh().draw()?;
+        chart.configure_mesh()
+            .light_line_style(ShapeStyle { color: TRANSPARENT, filled: false, stroke_width: 0 })
+            .draw()?;
 
         chart
             .draw_series(LineSeries::new(
@@ -91,20 +110,302 @@ impl PlottingExample {
         Ok(())
     }
 
+    fn histogram_chart(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let root = self.graph.draw().unwrap();
+
+        let mut chart = ChartBuilder::on(&root)
+            .x_label_area_size(35)
+            .y_label_area_size(40)
+            .margin(25)
+            .caption("Histogram Test", ("sans-serif", 50.0).into_font())
+            .build_cartesian_2d((0u32..10u32).into_segmented(), 0u32..10u32)?;
+
+        chart
+            .configure_mesh()
+            .disable_x_mesh()
+            .bold_line_style(&WHITE.mix(0.3))
+            .y_desc("Count")
+            .x_desc("Bucket")
+            .axis_desc_style(("sans-serif", 15))
+            .draw()?;
+
+        let data = [
+            0u32, 1, 1, 1, 4, 2, 5, 7, 8, 6, 4, 2, 1, 8, 3, 3, 3, 4, 4, 3, 3, 3,
+        ];
+
+        chart.draw_series(
+            Histogram::vertical(&chart)
+                .style(RED.mix(0.5).filled())
+                .data(data.iter().map(|x: &u32| (*x, 1))),
+        )?;
+
+        Ok(())
+    }
+
+    fn mandelbrot_chart(&self) -> Result<(), Box<dyn std::error::Error>> {
+        use std::ops::Range;
+
+        let root = self.graph.draw().unwrap();
+
+        fn mandelbrot_set(
+            real: Range<f64>,
+            complex: Range<f64>,
+            samples: (usize, usize),
+            max_iter: usize,
+        ) -> impl Iterator<Item = (f64, f64, usize)> {
+            let step = (
+                (real.end - real.start) / samples.0 as f64,
+                (complex.end - complex.start) / samples.1 as f64,
+            );
+            return (0..(samples.0 * samples.1)).map(move |k| {
+                let c = (
+                    real.start + step.0 * (k % samples.0) as f64,
+                    complex.start + step.1 * (k / samples.0) as f64,
+                );
+                let mut z = (0.0, 0.0);
+                let mut cnt = 0;
+                while cnt < max_iter && z.0 * z.0 + z.1 * z.1 <= 1e10 {
+                    z = (z.0 * z.0 - z.1 * z.1 + c.0, 2.0 * z.0 * z.1 + c.1);
+                    cnt += 1;
+                }
+                return (c.0, c.1, cnt);
+            });
+        }
+
+        let mut chart = ChartBuilder::on(&root)
+            .margin(20)
+            .x_label_area_size(10)
+            .y_label_area_size(10)
+            .build_cartesian_2d(-2.1f64..0.6f64, -1.2f64..1.2f64)?;
+
+        chart
+            .configure_mesh()
+            .disable_x_mesh()
+            .disable_y_mesh()
+            .draw()?;
+
+        let plotting_area = chart.plotting_area();
+
+        let range = plotting_area.get_pixel_range();
+
+        let (pw, ph) = (range.0.end - range.0.start, range.1.end - range.1.start);
+        let (xr, yr) = (chart.x_range(), chart.y_range());
+
+        for (x, y, c) in mandelbrot_set(xr, yr, (pw as usize, ph as usize), 100) {
+            if c != 100 {
+                plotting_area.draw_pixel((x, y), &HSLColor(c as f64 / 100.0, 1.0, 0.5))?;
+            } else {
+                plotting_area.draw_pixel((x, y), &BLACK)?;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn multiple_plot(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let root = self.graph.draw().unwrap();
+        Ok(())
+    }
+
+    fn animated_chart(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let root = self.graph.draw().unwrap();
+
+        let time = self.plotting_data.borrow_mut()
+            .animation_start
+            .as_ref()
+            .map(|t| t.elapsed())
+            .unwrap_or(Duration::new(0, 0));
+
+        let offset = (time.as_millis() as f64) / 1000.0;
+        let x_spec = (0.0+offset)..(5.0+offset);
+        
+        let sin_x_values = (0..=250)
+            .map(|x| x as f64 / 50.0)
+            .map(|x| ((x+offset), (x+offset).sin()));
+
+        let cos_x_values = (0..=250)
+            .map(|x| x as f64 / 50.0)
+            .map(|x| ((x+offset), (x+offset).cos()));
+
+        let mut chart = ChartBuilder::on(&root)
+            .caption("SIN & COS", ("sans-serif", 50).into_font())
+            .margin(15)
+            .x_label_area_size(30)
+            .y_label_area_size(30)
+            .build_cartesian_2d(x_spec, -1.5f64..1.5f64)?;
+
+        chart.configure_mesh()
+            .x_labels(20)
+            .y_labels(10)
+            .disable_mesh()
+            .draw()?;
+
+        chart
+            .draw_series(LineSeries::new(sin_x_values, &RED))?
+            .label("y = sin(x)")
+            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+        chart
+            .draw_series(LineSeries::new(cos_x_values, &BLUE))?
+            .label("y = cos(x)")
+            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLUE));
+
+        chart
+            .configure_series_labels()
+            .background_style(&WHITE.mix(0.8))
+            .border_style(&BLACK)
+            .draw()?;
+
+
+        Ok(())
+    }
+
+    fn interactive_chart(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let root = self.graph.draw().unwrap();
+
+        let mut chart = ChartBuilder::on(&root)
+            .caption("y=x*2", ("sans-serif", 50).into_font())
+            .margin(50)
+            .x_label_area_size(30)
+            .y_label_area_size(30)
+            .build_cartesian_2d(-100..100, -200..200)?;
+
+
+        chart.configure_mesh()
+            .light_line_style(ShapeStyle { color: TRANSPARENT, filled: false, stroke_width: 0 })
+            .draw()?;
+
+        
+        chart
+            .draw_series(LineSeries::new(
+                [-100, -50, 0, 50, 100].iter().map(|&x| (x, x * 2)),
+                &RED,
+            ))?
+            .label("y = x*2")
+            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+        chart.configure_series_labels()
+            .border_style(&BLACK)
+            .draw()?;
+        
+        
+        // As far as I know, there's no way to fetch the margin in pixels within a chart, so you have to use trial and error
+        // 80 & 130 seems to be good enough for this case
+        let (x, _) = nwg::GlobalCursor::local_position(&self.graph, None);
+        let max_x = self.graph.size().0 as i32;
+        let percent = (x-80) as f32 / (max_x-130) as f32;
+        let value = (((percent - 0.5) * 200.0) as i32).clamp(-100, 100);
+
+        chart.draw_series(PointSeries::of_element(
+            [value].iter().map(|&x| (x, x * 2)),
+            5,
+            ShapeStyle::from(&RED).filled(),
+            &|coord, size, style| {
+                EmptyElement::at(coord)
+                    + Circle::new((0, 0), size, style)
+                    + Text::new(format!("{:?}", coord), (0, 15), ("sans-serif", 15))
+            },
+        ))?;
+
+        chart
+            .configure_series_labels()
+            .background_style(&WHITE.mix(0.8))
+            .border_style(&BLACK)
+            .draw()?;
+
+        Ok(())
+    }
+
+    fn kotch_snowflake(&self) ->  Result<(), Box<dyn std::error::Error>> {
+        let root = self.graph.draw().unwrap();
+
+        fn snowflake_iter(points: &[(f64, f64)]) -> Vec<(f64, f64)> {
+            let mut ret = vec![];
+            for i in 0..points.len() {
+                let (start, end) = (points[i], points[(i + 1) % points.len()]);
+                let t = ((end.0 - start.0) / 3.0, (end.1 - start.1) / 3.0);
+                let s = (
+                    t.0 * 0.5 - t.1 * (0.75f64).sqrt(),
+                    t.1 * 0.5 + (0.75f64).sqrt() * t.0,
+                );
+                ret.push(start);
+                ret.push((start.0 + t.0, start.1 + t.1));
+                ret.push((start.0 + t.0 + s.0, start.1 + t.1 + s.1));
+                ret.push((start.0 + t.0 * 2.0, start.1 + t.1 * 2.0));
+            }
+            ret
+        }
+
+        let mut chart = ChartBuilder::on(&root)
+            .caption("Koch's Snowflake", ("sans-serif", 50))
+            .build_cartesian_2d(-2.0..2.0, -1.5..1.5)?;
+
+        let mut snowflake_vertices = {
+            let mut current: Vec<(f64, f64)> = vec![
+                (0.0, 1.0),
+                ((3.0f64).sqrt() / 2.0, -0.5),
+                (-(3.0f64).sqrt() / 2.0, -0.5),
+            ];
+            for _ in 0..6 {
+                current = snowflake_iter(&current[..]);
+            }
+            current
+        };
+
+        chart.draw_series(std::iter::once(Polygon::new(
+            snowflake_vertices.clone(),
+            &RED.mix(0.2),
+        )))?;
+        snowflake_vertices.push(snowflake_vertices[0]);
+        chart.draw_series(std::iter::once(PathElement::new(snowflake_vertices, &RED)))?;
+
+        Ok(())
+    }
+
+    fn bitmap(&self) ->  Result<(), Box<dyn std::error::Error>> {
+
+        Ok(())
+    }
+
     fn draw_graph(&self) {
         let index = self.example_list.selection().unwrap_or(0);
+
         let result = match index {
             0 => self.simple_chart(),
-            1 => self.simple_chart(),
-            2 => self.simple_chart(),
-            3 => self.simple_chart(),
-            4 => self.simple_chart(),
+            1 => self.histogram_chart(),
+            2 => self.mandelbrot_chart(),
+            3 => self.multiple_plot(),
+            4 => self.animated_chart(),
+            5 => self.interactive_chart(),
+            6 => self.kotch_snowflake(),
+            7 => self.bitmap(),
             _ => unreachable!(),
         };
 
         if let Err(e) = result {
             let msg = format!("Error drawing chart: {:?}", e);
             nwg::modal_error_message(&self.window, "Error", &msg);
+        }
+    }
+
+    fn check_animate(&self) {
+        let index = self.example_list.selection().unwrap_or(0);
+        if index == 4 {
+            self.plotting_data.borrow_mut().animation_start = Some(Instant::now());
+            self.animation_timer.start();
+        } else {
+            self.animation_timer.stop();
+        }
+    }
+
+    fn update_interactive(&self) {
+        let index = self.example_list.selection().unwrap_or(0);
+        if index == 5 {
+            let result = self.interactive_chart();
+            if let Err(e) = result {
+                let msg = format!("Error drawing chart: {:?}", e);
+                nwg::modal_error_message(&self.window, "Error", &msg);
+            }
         }
     }
 
--- native-windows-gui/src/win32/plotters_d2d.rs
+++ native-windows-gui/src/win32/plotters_d2d.rs
@@ -10,7 +10,7 @@ use super::base_helper::to_utf16;
 use std::{cell::{Ref, RefCell, RefMut}, collections::HashMap, mem, ptr};
 
 use plotters::prelude::DrawingBackend;
-use plotters_backend::{DrawingErrorKind, BackendColor, BackendStyle, BackendTextStyle};
+use plotters_backend::{DrawingErrorKind, BackendColor, BackendStyle, BackendTextStyle, BackendCoord};
 
 
 
@@ -101,10 +101,20 @@ impl std::error::Error for PlottersError {
 
 }
 
+struct PixelBitmap {
+    memory: Vec<u8>,
+    size: (u32, u32),
+    bitmap: *mut ID2D1Bitmap
+}
 
 struct Target {
     render_target: *mut ID2D1HwndRenderTarget,
     brushes: HashMap<Color, *mut ID2D1SolidColorBrush>,
+
+    // Target to draw pixels if draw_pixel is called
+    pixel_bitmap: Option<PixelBitmap>,
+    write_pixels: bool,
+
     size: (u32, u32),
     last_error: i32,
 }
@@ -142,6 +152,51 @@ impl Target {
 
         *brush
     }
+
+    pub(crate) fn allocate_pixel_bitmap(&mut self, renderer: *mut ID2D1Factory) {
+        use winapi::shared::dxgiformat::DXGI_FORMAT_R8G8B8A8_UNORM;
+        use winapi::um::dcommon::{D2D1_PIXEL_FORMAT, D2D1_ALPHA_MODE_PREMULTIPLIED};
+
+        if let Some(bitmap) = self.pixel_bitmap.as_ref() {
+            let old_size = bitmap.size;
+            let new_size = self.size;
+            if old_size == new_size {
+                return;
+            }
+        }
+
+        let (width, height) = (self.size.0 as usize, self.size.1 as usize);
+        let pixel_size = 4;
+
+        let bitmap = unsafe {
+            let mut bitmap = ptr::null_mut();
+
+            let mut dpi_x = 0.0;
+            let mut dpi_y = 0.0;
+            (&*renderer).GetDesktopDpi(&mut dpi_x, &mut dpi_y);
+
+            (&*self.render_target).CreateBitmap(
+                D2D1_SIZE_U { width: width as _, height: height as _ },
+                ptr::null(),
+                0,
+                &D2D1_BITMAP_PROPERTIES {
+                    pixelFormat: D2D1_PIXEL_FORMAT { format: DXGI_FORMAT_R8G8B8A8_UNORM, alphaMode: D2D1_ALPHA_MODE_PREMULTIPLIED },
+                    dpiX: dpi_x,
+                    dpiY: dpi_y,
+                },
+                &mut bitmap,
+            );
+
+            bitmap
+        };
+
+        self.pixel_bitmap = Some(PixelBitmap {
+            memory: vec![0; width*height*pixel_size],
+            size: self.size,
+            bitmap
+        });
+    }
+
 }
 
 impl Drop for Target {
@@ -188,7 +243,36 @@ impl PlottersBackend {
 
     pub(crate) fn end_draw(&self) {
         let result = unsafe { 
-            let target = self.target();
+            let mut target = self.target_mut();
+
+            // Writes the pixel bitmap if needed
+            if target.write_pixels {
+                if let Some(bitmap) = target.pixel_bitmap.as_ref() {
+                    let (width, height) = bitmap.size;
+
+                    let copy_rect = D2D1_RECT_U {
+                        top: 0, left: 0,
+                        bottom: height, right: width,
+                    };
+
+                    let draw_rect = D2D1_RECT_F {
+                        top: 0.0, left: 0.0,
+                        bottom: height as f32, right: width as f32,
+                    };
+
+                    (&*bitmap.bitmap).CopyFromMemory(&copy_rect, bitmap.memory.as_ptr() as _, width*4);
+                    (&*target.render_target).DrawBitmap(
+                        bitmap.bitmap,
+                        &draw_rect,
+                        1.0,
+                        D2D1_BITMAP_INTERPOLATION_MODE_LINEAR,
+                        &draw_rect
+                    );
+                }
+
+                target.write_pixels = false;
+            }
+
             (&*target.render_target).EndDraw(ptr::null_mut(), ptr::null_mut())
         };
 
@@ -214,6 +298,9 @@ impl PlottersBackend {
         if target.size != new_size || target.last_error == D2DERR_RECREATE_TARGET {
             *target = unsafe { build_render_target(handle, &mut *self.renderer)? };
         }
+
+
+        target.allocate_pixel_bitmap(self.renderer);
 
         Ok(())
     }
@@ -307,9 +394,27 @@ impl<'a> DrawingBackend for &'a PlottersBackend {
 
     fn draw_pixel(
         &mut self,
-        _point: (i32, i32),
-        _color: BackendColor
+        point: (i32, i32),
+        color: BackendColor
     ) -> Result<(), DrawingErrorKind<Self::ErrorType>> {
+        let mut target = self.target.borrow_mut();
+
+        // Allocate the pixel bitmap if it was not allocated before
+        target.allocate_pixel_bitmap(self.renderer);
+
+        // Tells the API to write the pixel bitmap on top of the canvas at the end of the drawing
+        target.write_pixels = true;
+
+        // Write the pixel to the bitmap
+        let (x, y) = (point.0 as usize, point.1 as usize);
+        let width = target.size.0 as usize;
+        let pixels = target.pixel_bitmap.as_mut().unwrap();
+        let pixel_offset = ((width * y * 4) + (x * 4)).max(0);
+        pixels.memory[pixel_offset] = color.rgb.0;
+        pixels.memory[pixel_offset+1] = color.rgb.1;
+        pixels.memory[pixel_offset+2] = color.rgb.2;
+        pixels.memory[pixel_offset+3] = 255;
+
         Ok(())
     }
 
@@ -451,11 +556,47 @@ impl<'a> DrawingBackend for &'a PlottersBackend {
         Ok(())
     }
 
-    fn fill_polygon<S: BackendStyle, I>(
+    fn fill_polygon<S: BackendStyle, I: IntoIterator<Item = BackendCoord>>(
         &mut self,
-        _vert: I,
-        _style: &S
+        vert: I,
+        style: &S
     ) -> Result<(), DrawingErrorKind<Self::ErrorType>> {
+
+        let mut target = self.target_mut();
+        let brush = target.fetch_brush(Color::from(&style.color()));
+
+        unsafe {
+            let fact = &*self.renderer;
+            let mut path = ptr::null_mut();
+            let mut sink = ptr::null_mut();
+            
+            
+            fact.CreatePathGeometry(&mut path);
+            (&*path).Open(&mut sink);
+
+            let mut vertex_iter = vert.into_iter();
+            let (px, py) = vertex_iter.next().unwrap_or((0, 0));
+            (&*sink).BeginFigure(D2D1_POINT_2F { x: px as f32, y: py as f32 }, D2D1_FIGURE_BEGIN_FILLED);
+
+            for (px, py) in vertex_iter {
+                (&*sink).AddLine(D2D1_POINT_2F { x: px as f32, y: py as f32 });
+            }
+
+            (&*sink).EndFigure(D2D1_FIGURE_END_CLOSED);
+            (&*sink).Close();
+
+
+            (&*target.render_target).FillGeometry(
+                path as _, 
+                brush as _,
+                ptr::null_mut(),
+            );
+
+
+            (&*sink).Release();
+            (&*path).Release();
+        }
+        
         Ok(())
     }
 
@@ -603,6 +744,8 @@ unsafe fn build_render_target(hwnd: HWND, factory: &mut ID2D1Factory) -> Result<
         Ok(Target {
             render_target,
             brushes: Default::default(),
+            pixel_bitmap: None,
+            write_pixels: false,
             size: (width, height),
             last_error: S_OK
         })
